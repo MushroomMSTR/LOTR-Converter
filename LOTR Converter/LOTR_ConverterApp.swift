03a1d005aa5478bf8ededd1a4cd8d944f0de4ce6
NazarStf <hworker98@gmail.com>
2023-07-10 07:39:29 +0300

    Initial Commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by NazarStf on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
