6ba64f319f239905efe2d2c36aee5a8d6970bede
NazarStf <hworker98@gmail.com>
2023-07-13 14:42:48 +0300

    Lesson 3 - Adding Basic Views to our design

--- LOTR Converter/ContentView.swift
+++ LOTR Converter/ContentView.swift
@@ -11,12 +11,19 @@ struct ContentView: View {
     var body: some View {
         ZStack {
 			// Back
-			
+			Image("background")
+				.resizable()
+				.ignoresSafeArea()
 			VStack {
 				// Prancing pony image
-				
+				Image("prancingpony")
+					.resizable()
+					.scaledToFit()
+					.frame(height: 200)
 				// Currency exchange text
-				
+				Text("Currency Exchange")
+					.font(.largeTitle)
+					.foregroundColor(.white)
 				// Currency conversion section
 				HStack {
 					// Left
@@ -24,31 +31,59 @@ struct ContentView: View {
 						// Currency
 						HStack {
 							// Currency Image
-							
+							Image("silverpiece")
+								.resizable()
+								.scaledToFit()
+								.frame(height: 33)
 							// Currency text
+							Text("Silver Piece")
+								.font(.headline)
+								.foregroundColor(.white)
 						}
 						// Text field
+						Text("Text field")
 					}
 					
 					// "="
-					
+					Image(systemName: "equal")
+						.font(.largeTitle)
+						.foregroundColor(.white)
 					// Right
 					VStack {
 						// Currency
 						HStack {
 							// Currency text
-							
+							Text("Gold Piece")
+								.font(.headline)
+								.foregroundColor(.white)
 							// Currency Image
-							
+							Image("goldpiece")
+								.resizable()
+								.scaledToFit()
+								.frame(height: 33)
 						}
 						// Text field
+						Text("Text field")
 					}
 				}
 				
+				Spacer()
+				
 				// Info button
+				HStack {
+					Spacer()
+					Button {
+						// Display exchange info screen
+					} label: {
+						Image(systemName: "info.circle.fill")
+					}
+					.font(.largeTitle)
+					.foregroundColor(.white)
+					.padding(.trailing, 5)
+				}
 			}
-        }
-    }
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
